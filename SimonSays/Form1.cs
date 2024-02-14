30f5e7b64721363745ec32897862b078141c11b4
Grady Rueffer School <113617132+GradyRuefferOutlook@users.noreply.github.com>
2024-02-14 11:25:16 -0500

    Started

--- SimonSays/Form1.cs
+++ SimonSays/Form1.cs
@@ -14,8 +14,16 @@ namespace SimonSays
 {
     public partial class Form1 : Form
     {
-        //TODO: create a List to store the pattern. Must be accessable on other screens
+        public static List<PointF> circList = new List<PointF>();
+        public static Int32 circDrawTime = 0;
+        public static int radius = 50;
+        public static PointF[] circlePointsBor;
+        public static PointF[] circlePointsInsBor;
+        public static PointF[] circlePointsIns;
+        public static PointF[] circlePointsOuts;
 
+        //TODO: create a List to store the pattern. Must be accessable on other screens
+        public static List<Int64> protectionPattern = new List<Int64>();
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +31,29 @@ namespace SimonSays
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //TODO: Launch MenuScreen
+            ScreenChanger(new MenuScreen(), this);
+        }
+
+        public static void ScreenChanger(UserControl next, object sender)
+        {
+            Form f;
+            UserControl current = new UserControl();
+
+            if (sender is Form)
+            {
+                f = (Form)sender;
+            }
+
+            else
+            {
+                current = (UserControl)sender;
+                f = current.FindForm();
+                f.Controls.Remove(current);
+            }
+
+            next.Location = new Point((f.Width - next.Width) / 2, (f.Height - next.Height) / 2);
+
+            f.Controls.Add(next);
         }
     }
 }
--- SimonSays/MenuScreen.cs
+++ SimonSays/MenuScreen.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace SimonSays
 {
@@ -14,6 +15,7 @@ namespace SimonSays
         public MenuScreen()
         {
             InitializeComponent();
+            Refresh();
         }
 
         private void newButton_Click(object sender, EventArgs e)
@@ -25,6 +27,92 @@ namespace SimonSays
         private void exitButton_Click(object sender, EventArgs e)
         {
             //TODO: end the application
+        }
+
+        void DrawUserInput(int radius)
+        {
+            DrawCircle(radius + 25);
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circList[i] = new PointF(Form1.circList[i].X + radius - 10, Form1.circList[i].Y + radius + (this.Height / 2) + 50);
+            }
+            Form1.circlePointsBor = new PointF[Form1.circList.Count];
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circlePointsBor[i] = new PointF(Form1.circList[i].X, Form1.circList[i].Y);
+            }
+
+            DrawCircle(radius + 15);
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circList[i] = new PointF(Form1.circList[i].X + radius - 10, Form1.circList[i].Y + radius + (this.Height / 2) + 50);
+            }
+            Form1.circlePointsOuts = new PointF[Form1.circList.Count];
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circlePointsOuts[i] = new PointF(Form1.circList[i].X, Form1.circList[i].Y);
+            }
+
+            DrawCircle(radius - 40);
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circList[i] = new PointF(Form1.circList[i].X + radius - 10, Form1.circList[i].Y + radius + (this.Height / 2) + 50);
+            }
+            Form1.circlePointsInsBor = new PointF[Form1.circList.Count];
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circlePointsInsBor[i] = new PointF(Form1.circList[i].X, Form1.circList[i].Y);
+            }
+
+            DrawCircle(radius - 50);
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circList[i] = new PointF(Form1.circList[i].X + radius - 10, Form1.circList[i].Y + radius + (this.Height / 2) + 50);
+            }
+            Form1.circlePointsIns = new PointF[Form1.circList.Count];
+            for (int i = 0; i < Form1.circList.Count; i++)
+            {
+                Form1.circlePointsIns[i] = new PointF(Form1.circList[i].X, Form1.circList[i].Y);
+            }
+        }
+
+        void DrawCircle(int radius)
+        {
+            Form1.circList.Clear();
+            for (double x = -radius; x <= radius; x += 0.1)
+            {
+                if (x == radius)
+                {
+                    Form1.circList.Add(new PointF(Convert.ToSingle(x), 0));
+                }
+                else
+                {
+                    Form1.circList.Add(new PointF(Convert.ToSingle(x), Convert.ToSingle(Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(x, 2)))));
+                }
+            }
+            for (double x = radius; x >= -radius; x -= 0.1)
+            {
+                if (x == -radius)
+                {
+                    Form1.circList.Add(new PointF(Convert.ToSingle(x), 0));
+                }
+                else
+                {
+                    Form1.circList.Add(new PointF(Convert.ToSingle(x), -1 * Convert.ToSingle(Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(x, 2)))));
+                }
+            }
+            Form1.circList.Add(Form1.circList[0]);
+        }
+
+        private void MenuScreen_Paint(object sender, PaintEventArgs e)
+        {
+            DrawUserInput((this.Width / 2) + 10);
+            e.Graphics.FillPolygon(new SolidBrush(Color.FromArgb(255, 0, 0, 0)), Form1.circlePointsBor);
+            e.Graphics.DrawPolygon(new Pen(Color.Gray), Form1.circlePointsBor);
+            e.Graphics.DrawPolygon(new Pen(Color.Gray), Form1.circlePointsOuts);
+            e.Graphics.DrawPolygon(new Pen(Color.Gray), Form1.circlePointsInsBor);
+            e.Graphics.DrawPolygon(new Pen(Color.Gray), Form1.circlePointsIns);
+            e.Graphics.FillPolygon(new SolidBrush(Color.FromArgb(255, 0 , 0, 125)), Form1.circlePointsIns);
         }
     }
 }
